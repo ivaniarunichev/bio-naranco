c590ad25587cfd8b857d66747fb61632b5445bf6
Iván Iarunichev <ivani94@educastur.es>
2025-10-07 19:24:57 +0200

    Cambio LabData CLASE

---
+++ src/LabData.java
@@ -0,0 +1,44 @@
+import java.io.Serializable;
+
+class LabData implements Serializable {
+        //private HashMap<...> pacientes;
+        //    private HashMap<...> tecnicos;
+        //    private List<...> muestras;
+
+        /* Carga los datos en los arrays leyendo los ficheros de entrada en ./data/in
+        public ...
+
+        leerPacientes() {
+
+        }
+
+        public ...
+
+        leerTecnicos() {
+
+        }
+
+        public ...
+
+        leerMuestras() {
+
+        }
+
+
+         * Escribe los datos generando los ficheros de salida en ./data/out
+
+
+        public void generarMuestrasConsolidado() {
+
+        }
+
+        public void generarMuestrasAppExterna() {
+
+        }
+
+        public void generarSerializado() {
+
+        }
+    }
+    */
+}
