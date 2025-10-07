449c6854421b76594c5037dc62ce3d05bf03bf09
CarlosPV-oss <carlospv18@educastur.es>
2025-10-07 19:04:24 +0200

    labData

---
+++ src/labData.java
@@ -0,0 +1,28 @@
+import java.io.Serializable;
+
+class LabData implements Serializable {
+    //private HashMap<...> pacientes;
+    //    private HashMap<...> tecnicos;
+    //    private List<...> muestras;
+/**Carga los datos en los arrays leyendo los ficheros de entrada en ./data/in
+ */
+    public ... leerPacientes(){
+//TODO
+    }
+    public ... leerTecnicos(){
+//TODO
+    }
+    public ... leerMuestras(){
+//TODO
+    }
+    /** Escribe los datos generando los ficheros de salida en ./data/out
+     */
+    public void generarMuestrasConsolidado(){
+//TODO
+    }
+    public void generarMuestrasAppExterna(){
+//TODO
+    }
+    public void generarSerializado(){
+//TODO
+    }
